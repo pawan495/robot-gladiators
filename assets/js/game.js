6650e8bb287aabf981b49d316e1b4195343923a5
pawan495 <pawanpreetkaur495@gmail.com>
2021-12-18 21:03:16 -0500

    add first code

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+// fight();
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+window.alert("This is an alert! JavaScript is running!");
+
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  var stringDataType = "This is a string, which is a fancy way to say text";
+  var name = "your name";
+  console.log(name);
--- game.js
+++ game.js
@@ -1,1 +1,10 @@
+fight();
+window.prompt("What is your robot's name?");
+
 window.alert("This is an alert! JavaScript is running!");
+
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  
