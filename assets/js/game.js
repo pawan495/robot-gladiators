1dd240c6cb3d9aaea370f09641aa0de6d8f4dd8a
pawan495 <pawanpreetkaur495@gmail.com>
2022-01-04 23:23:59 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,7 +65,8 @@ var fight = function(enemyName) {
     }
   }
 };
-
+// function to start a new game
+var startGame = function() {
 for (var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
@@ -82,3 +83,31 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+};
+// play again
+  // startGame();
+// function to end the entire game
+var endGame = function() {
+  // if player is still alive, player wins!
+  if(playerHealth>0){
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  }
+  else{
+    window.alert("You've lost your robot in battle.");
+  }
+}
+  // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+
+  window.alert("The game has now ended. Let's see how you did!");
+};
+
+
+
